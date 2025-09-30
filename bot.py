d16b1c2f8eba2ad4c2f5e0d5fcd968437d71d1a3
il88li <yl9667383@gmail.com>
2025-09-30 23:29:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 # بيانات البوت
 API_ID = 23656977
 API_HASH = '49d3f43531a92b3f5bc403766313ca1e'
-BOT_TOKEN = '8228285723:AAHwfs_M8b4bnxgJPmjMNtR1nm0P6yoLEDk'
+BOT_TOKEN = '8052900952:AAGCbBxSw5drwFg-lxsg905wNZejZzfSWsQ'
 
 # رابط الويب هووك الجديد
 WEBHOOK_URL = 'https://trans-2-77.onrender.com'
